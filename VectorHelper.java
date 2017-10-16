c07ffbcf376fdd07ee43e2592c5ff447806b3d8d
Amine Bouchebaba <ABouchebaba@outlook.com>
2017-10-16 07:23:27 +0200

    added reverse method

--- VectorHelper.java
+++ VectorHelper.java
@@ -1,14 +1,27 @@
+
+
 package com.company;
+
 
 import java.util.ArrayList;
 import java.util.List;
+
+/**
+ * this class implements a Vector of integers.
+ * @author Bouchebaba/Tayeb Cherif
+ * @version 1.0
+ *
+ */
 
 public class VectorHelper {
 
     public int vector[] ;
 
 
-
+    /**
+     * This method creates a vector from an undefined number of integers.
+     * @param vect undefined number of integers used to create the vector.
+     */
     public VectorHelper(int ... vect){
 
         vector = new int[vect.length];
@@ -19,6 +32,9 @@ public class VectorHelper {
 
     }
 
+    /**
+     * This method sorts the vector in a raising order.
+     */
 
     public void sort(){
             boolean sorted = false, permut = false;
@@ -47,12 +63,31 @@ public class VectorHelper {
 
     }
 
+    /**
+     * This method adds a vector to the caller vector.
+     * @param vect Vector to be added to the caller vector.
+     * @throws Exception Throws an Exception if the vectors are not equally sized.
+     */
+
     public void add(VectorHelper vect) throws  Exception{
         if (vect.vector.length != this.vector.length)
             throw new Exception();
 
         for (int i =0; i<this.vector.length; i++){
             this.vector[i] += vect.vector[i];
+        }
+    }
+
+    /**
+     *
+     */
+    public void reverse(){
+        int l = vector.length-1;
+
+        for (int i=0; i< l/2; i++){
+            int tmp = vector[i];
+            vector[i] = vector[l-i];
+            vector[l-i] = tmp;
         }
     }
 
